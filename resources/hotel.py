b86a25637cb30cca54d3c1f93eed5f002904c240
Rafael Jardim <rafaelj13.lima@gmail.com>
2024-08-23 15:28:13 -0300

    Criação da classe modelo

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 hoteis = [
     {
@@ -29,11 +30,11 @@ class Hoteis(Resource):
         return {'hoteis': hoteis}
     
 class Hotel(Resource):
-    argumentos = reqparse.RequestParser()
-    argumentos.add_argument('nome')
-    argumentos.add_argument('estrelas')
-    argumentos.add_argument('diaria')
-    argumentos.add_argument('cidade')
+    atributos = reqparse.RequestParser()
+    atributos.add_argument('nome')
+    atributos.add_argument('estrelas')
+    atributos.add_argument('diaria')
+    atributos.add_argument('cidade')
 
     def find_hotel(hotel_id):
         for hotel in hoteis:
@@ -47,18 +48,16 @@ class Hotel(Resource):
             return hotel
         return {'message': 'Hotel not found'}, 404
     def post(self, hotel_id):
-
-        dados = Hotel.argumentos.parse_args()
-
-        novo_hotel = {'hotel_id': hotel_id, **dados}
-
+        dados = Hotel.atributos.parse_args()
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hoteis.append(novo_hotel)
         return novo_hotel, 200
     def put(self, hotel_id):
 
-        dados = Hotel.argumentos.parse_args()
-        novo_hotel = {'hotel_id': hotel_id, **dados}
-
+        dados = Hotel.atributos.parse_args()
+        hotel_objeto = {'hotel_id': hotel_id, **dados}
+        novo_hotel = hotel_objeto.json()
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
             hotel.update(novo_hotel)
