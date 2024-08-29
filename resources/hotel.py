34f7648494604aedda9c6cdb8902e98a70b6cd8b
Rafael Jardim <rafaelj13.lima@gmail.com>
2024-08-29 10:18:30 -0300

    Adicionando filtros (Erro 400)

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,49 +1,114 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 from flask_jwt_extended import jwt_required
+import sqlite3
+import json
+
+def normalize_path_params(cidade=None,
+                          estrelas_min=0,
+                          estrelas_max=5,
+                          diaria_min=0,
+                          diaria_max=10000,
+                          limit=50,
+                          offset=0, **dados):
+    if cidade:
+        return {
+            'estrelas_min': estrelas_min,
+            'estrelas_max': estrelas_max,
+            'diaria_min': diaria_min,
+            'diaria_max': diaria_max,
+            'cidade': cidade,
+            'limit': limit,
+            'offset': offset}
+    return {
+        'estrelas_min': estrelas_min,
+        'estrelas_max': estrelas_max,
+        'diaria_min': diaria_min,
+        'diaria_max': diaria_max,
+        'limit': limit,
+        'offset': offset}
+
+path_params = reqparse.RequestParser()
+path_params.add_argument('cidade', type=str)
+path_params.add_argument('estrelas_min', type=float)
+path_params.add_argument('estrelas_max', type=float)
+path_params.add_argument('diaria_min', type=float)
+path_params.add_argument('diaria_max', type=float)
+path_params.add_argument('limit', type=float)
+path_params.add_argument('offset', type=float)
 
 class Hoteis(Resource):
     def get(self):
-        # Rota para listar todos os hoteis cadastrados
+        connection = sqlite3.connect('banco.db')
+        cursor = connection.cursor()
+
+        dados = path_params.parse_args()
+        dados_validos = {chave:dados[chave] for chave in dados if dados[chave] is not None}
+        parametros = normalize_path_params(**dados_validos)
+
+        if not parametros.get('cidade'):
+            consulta = "SELECT * FROM hoteis \
+            WHERE (estrelas >= ? and estrelas <= ?) \
+            and (diaria >= ? and diaria <= ?) \
+            LIMIT ? OFFSET ?"
+            tupla = tuple([parametros[chave] for chave in parametros])
+            resultado = cursor.execute(consulta, tupla)
+        else:
+            consulta = "SELECT * FROM hoteis \
+            WHERE (estrelas >= ? and estrelas <= ?) \
+            and (diaria >= ? and diaria <= ?) \
+            and cidade = ? LIMIT ? OFFSET ?"
+            tupla = tuple([parametros[chave] for chave in parametros])
+            resultado = cursor.execute(consulta, tupla)
+
+        hoteis = []
+        for linha in resultado:
+            hoteis.append({
+                'hotel_id': linha[0],
+                'nome': linha[1],
+                'estrelas': linha[2],
+                'diaria': linha[3],
+                'cidade': linha[4]
+            })
+
         # Executa a query SELECT * FROM Hoteis
-        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
+        return {'hoteis': hoteis}
+
 
 class Hotel(Resource):
     atributos = reqparse.RequestParser()
     atributos.add_argument('nome', type=str, required=True, help="The field 'nome' cannot be left blank!")
-    atributos.add_argument('estrelas', type = float, required=True, help="The field 'estrelas' cannot be left blank!")
+    atributos.add_argument('estrelas')
     atributos.add_argument('diaria')
     atributos.add_argument('cidade')
 
     def get(self, hotel_id):
         # Rota para buscar um hotel pelo ID
-        # Executa a query SELECT * FROM Hoteis WHERE hotel_id = hotel_id
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
             return hotel.json()
         return {'message': 'Hotel not found'}, 404
-    
+
     @jwt_required()
     def post(self, hotel_id):
         # Rota para criar um novo hotel
-        # Executa a query INSERT INTO Hoteis (hotel_id, nome, estrelas, diaria, cidade) VALUES (hotel_id, nome, estrelas, diaria, cidade)
         if HotelModel.find_hotel(hotel_id):
-            return {"message": "Hotel id '{}' already exists.".format(hotel_id)},400
+            return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400
         
         dados = Hotel.atributos.parse_args()
+        print(f"Dados recebidos para POST: {dados}")
         hotel = HotelModel(hotel_id, **dados)
         try:
             hotel.save_hotel()
         except:
             return {"message": "An internal error occurred while inserting the hotel."}, 500
-        hotel.save_hotel()
-        return hotel.json()
-    
+        return hotel.json(), 201
+
     @jwt_required()
     def put(self, hotel_id):
         # Rota para atualizar um hotel pelo ID
-        # Executa a query UPDATE Hoteis SET nome = nome, estrelas = estrelas, diaria = diaria, cidade = cidade WHERE hotel_id = hotel_id
         dados = Hotel.atributos.parse_args()
+        print(f"Dados recebidos para PUT: {dados}")
         hotel_encontrado = HotelModel.find_hotel(hotel_id)
         if hotel_encontrado:
             hotel_encontrado.update_hotel(**dados)
@@ -56,11 +121,10 @@ class Hotel(Resource):
             return {"message": "An internal error occurred while inserting the hotel."}, 500
         hotel.save_hotel()
         return hotel.json(), 201
-    
+
     @jwt_required()
     def delete(self, hotel_id):
         # Rota para deletar um hotel pelo ID
-        # Executa a query DELETE FROM Hoteis WHERE hotel_id = hotel_id
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
             try:
